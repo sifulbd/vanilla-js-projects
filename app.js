77a5415b6cec6bdcdeeee76f3df959cbb8396a93
Siful Islam <sifulsmm@gmail.com>
2020-06-13 13:36:10 +0600

    reduce

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
 const arr = [
     {
-        title: 'hello title',
+        title: 'hlo title',
         desc: 'lorem ipsum doler',
         author: ['mia i', 'alex dorgon']
     },
     {
-        title: 'hello title two',
+        title: 'Mr hello title two',
         desc: 'lorem ipsum doler',
         author: ['john i', 'alex d']
     },
     {
-        title: 'hello title three',
+        title: 'Mr. hllo title three',
         desc: 'lorem ipsum doler',
-        author: ['james nam', 'alex alim']
+        author: ['james nam', 'alex']
+    },
+    {
+        title: 'Mrs. hllo title three',
+        desc: 'lorem ipsum doler',
+        author: ['james nam', 'alex']
     },
 ];
+let movies = [
+    'Mrs name',
+    'Mr pli'
+]
+// arr.forEach(function(item){
+//     const itemauth = item.author;
+//     const allAuthor = itemauth.join(' ');
+//     console.log(item.title,'author is ', allAuthor);
+// })
 
-arr.forEach(function(item){
-    const itemauth = item.author;
-    const allAuthor = itemauth.join(' ');
-    console.log(item.title,'author is ', allAuthor);
+// const hello = arr.find( b => (b.title.includes('hello') ));
+// const hello2 = movies.find(c => (c.indexOf('Mr') === 0));
+
+// const alexBook = arr.filter( b => (
+//     b.author.includes('alex')
+// ));
+// let query = 'Mrs';
+// const mrfind = arr.filter( b => {
+//     const title = b.title;
+//     return title.includes(query) 
+// })
+
+
+const nums = ['i am','a','boy'];
+const sunOfNums = nums.reduce((total, current) => {
+    return total * current;
 })
+console.log(sunOfNums);
+
