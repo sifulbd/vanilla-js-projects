9ba4894dbf0ff6849b946a5e122e88b9deefa354
Siful Islam <sifulsmm@gmail.com>
2020-06-25 11:02:21 +0600

    axio js

---
+++ apitest.js
@@ -0,0 +1,19 @@
+async function add(x, y){
+    if(typeof x !== 'number' || typeof y !== 'number'){
+        throw "Enter a valid number"
+    }
+    return x + y;
+}
+add("l","k").then( val => {
+    console.log("promise resolve", val)
+}).catch(err => {
+    console.log("promise rejected", err)
+})
+
+
+add(5,5).then( val => {
+    console.log("promise resolve", val)
+}).catch(err => {
+    console.log("promise rejected", err)
+})
+
