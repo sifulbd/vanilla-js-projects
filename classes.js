0e6799db26c6c7239347659fa73bed7c299d310e
Siful Islam <sifulsmm@gmail.com>
2020-06-28 12:34:49 +0600

    added classed

--- classes.js
+++ classes.js
@@ -1,67 +1,71 @@
-function makeColor(r,g,b) {
-    const color = {};
-    color.r = r;
-    color.g = g;
-    color.b = b;
-    color.rgb = function() {
-        const {r,g,b} = this;
-        return `rgb(${r}, ${g}, ${b})`;
-    };
-    color.hex = function() {
-        const {r,g,b} = this;
-        return (
-            '#' + ((1 << 24) + (r + 16) + (g << 8) + b).toString(16).slice(1));
-        }
-    return color;
-} 
+// git add -A
+// git commit -m "claases and prototype"
+// git push origin master
 
-const firstColor = makeColor(23,233,34);
-firstColor.rgb();
+// function makeColor(r,g,b) {
+//     const color = {};
+//     color.r = r;
+//     color.g = g;
+//     color.b = b;
+//     color.rgb = function() {
+//         const {r,g,b} = this;
+//         return `rgb(${r}, ${g}, ${b})`;
+//     };
+//     color.hex = function() {
+//         const {r,g,b} = this;
+//         return (
+//             '#' + ((1 << 24) + (r + 16) + (g << 8) + b).toString(16).slice(1));
+//         }
+//     return color;
+// } 
 
-function generateColor(r,g,b) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
-}
-generateColor.prototype.rgb = function() {
-    const {r,g,b} = this;
-    return `rgb(${r}, ${g}, ${b})`;
-}
-generateColor.prototype.hex = function() {
-    const {r,g,b} = this;
-    return ('#' + ((1 << 24) + (r + 16) + (g << 8) + b).toString(16).slice(1));
-}
-generateColor.prototype.rgba = function(a = 1.0) {
-    const {r,g,b} = this;
-    return `rgb(${r}, ${g}, ${b}, ${a})`;
-}
+// const firstColor = makeColor(23,233,34);
+// firstColor.rgb();
 
-color1 = new generateColor(23,255,43);
+// function generateColor(r,g,b) {
+//     this.r = r;
+//     this.g = g;
+//     this.b = b;
+// }
+// generateColor.prototype.rgb = function() {
+//     const {r,g,b} = this;
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
+// generateColor.prototype.hex = function() {
+//     const {r,g,b} = this;
+//     return ('#' + ((1 << 24) + (r + 16) + (g << 8) + b).toString(16).slice(1));
+// }
+// generateColor.prototype.rgba = function(a = 1.0) {
+//     const {r,g,b} = this;
+//     return `rgb(${r}, ${g}, ${b}, ${a})`;
+// }
+
+// color1 = new generateColor(23,255,43);
 
 
-class Dog{
-    constructor(r,g,b) {
-        this.r =r;
-        this.g = g;
-        this.b = b;
-    }
-    innerRGB() {
-        const {r,g,b} = this;
-        return `${r}, ${g}, ${b}`;
-    }
-    rgba(a = 1.0) {
-        return `rgb(${this.innerRGB()} , ${a})`;
-    }
-    rgb(){
-        return `rgb(${this.innerRGB()})`;
-    }
-    hex() {
-        const {r,g,b} = this;
-        return (
-            '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
-        );
-    }    
-}
+// class Dog{
+//     constructor(r,g,b) {
+//         this.r =r;
+//         this.g = g;
+//         this.b = b;
+//     }
+//     innerRGB() {
+//         const {r,g,b} = this;
+//         return `${r}, ${g}, ${b}`;
+//     }
+//     rgba(a = 1.0) {
+//         return `rgb(${this.innerRGB()} , ${a})`;
+//     }
+//     rgb(){
+//         return `rgb(${this.innerRGB()})`;
+//     }
+//     hex() {
+//         const {r,g,b} = this;
+//         return (
+//             '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+//         );
+//     }    
+// }
 
-const dog1 = new Dog(34,234,50);
-const white = new Dog(255,255,255);
+// const dog1 = new Dog(34,234,50);
+// const white = new Dog(255,255,255);
