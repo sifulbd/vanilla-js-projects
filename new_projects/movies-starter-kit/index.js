d9f9338c0a1ebc4f11837bb4a769654e878b0ae5
Siful Islam <sifulsmm@gmail.com>
2020-07-02 20:54:30 +0600

    movie files

--- new_projects/movies-starter-kit/index.js
+++ new_projects/movies-starter-kit/index.js
@@ -1,61 +1,85 @@
-const fetchApi = async (input) => {
+createAutoComplete({
+    root: document.querySelector('.autocomplete'),
+    renderOption(movie){
+      const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+      return `
+            <img src ="${imgSrc}" alt="" />
+            <h2>${movie.Title} </h2>  (${movie.Year})
+        `;
+    },
+    onOptionSelect(movie) {
+      isMovieSelect(movie);
+    },
+    inputValue(movie) {
+      return movie.Title
+    }, 
+    async fetchApi (input){
+      const response = await axios.get('http://www.omdbapi.com/', {
+          params: {
+              apikey: '785287b9',
+              s: input
+          }
+      });  
+      if(response.data.Error) {
+          return [];
+      }  
+      //return response data
+      return response.data.Search;
+  }
+});
+
+const isMovieSelect = async (movie) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '785287b9',
-            s: input
+            i: movie.imdbID
         }
     });
-
-    if(response.data.Error) {
-        return [];
-    }
-
-    //return response data
-    return response.data.Search;
+     document.querySelector('.movie-summery').innerHTML = movieTemplate(response.data);
+     console.log(response.data)
 }
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-        <label><b> Search for a Movie </b></label>
-        <input type="text" class="input">
-        <div class="dropdown">
-            <div class="dropdown-menu">
-            <div class="dropdown-content reuslts">
-                <a href="" class="dropdown-item">
-                    
-                </a>
+const movieTemplate = (movieDetails) => {
+    return `
+        <article class="media">
+            <figure class="media-left">
+                <p class="image">
+                    <img src="${movieDetails.Poster}" />
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content"
+                    <h2>${movieDetails.Title}</h2>
+                    <h4>${movieDetails.Genre}</h4>
+                    <p>${movieDetails.Plot}</p>
+                </div>
             </div>
-        </div>
-`
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const reusltWrapper = document.querySelector('.reuslts');
-
-const onInput = async event => {
-    const movies = await (fetchApi(event.target.value));
-
-    if(!movies.length) {
-        dropdown.classList.remove('is-active');
-        return;
-    }
-    reusltWrapper.innerHTML = '';
-    dropdown.classList.add('is-active');
-    for(let movie of movies) {
-        const option = document.createElement('a');
-        option.classList.add('dropdown-item');
-        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-        option.innerHTML = `
-            <img src ="${imgSrc}" alt="" />
-            <h2>${movie.Title}</h2>
-        `;
-        //add to html
-        reusltWrapper.appendChild(option);
-    }
+        </article>
+        <article class="notification is-success">
+            <p class="title">${movieDetails.Awards} </p>
+            <p class="subtitle"> Awards </p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetails.BoxOffice} </p>
+            <p class="subtitle"> Box Office </p>
+        </article>
+        <article class="notification is-danger">
+            <p class="title">${movieDetails.imdbRating} </p>
+            <p class="subtitle"> IMDB Ratings </p>
+        </article>
+        <article class="notification is-info">
+            <p class="title">${movieDetails.Language} </p>
+            <p class="subtitle"> Language </p>
+        </article>
+        <article class="notification is-warning">
+            <p class="title">${movieDetails.Actors} </p>
+            <p class="subtitle"> Actors </p>
+        </article>
+        <article class="notification is-danger is-light">
+            <p class="title">${movieDetails.Released} </p>
+            <p class="subtitle"> Released </p>
+        </article>
+    `
 }
-input.addEventListener('input', debounce(onInput), 500);
-document.addEventListener('click', event => {
-    if(!root.contains(event.target)) {
-        dropdown.classList.remove('is-active');
-    }
-})
+
+
